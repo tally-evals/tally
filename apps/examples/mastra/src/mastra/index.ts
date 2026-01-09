5245886dff3b107f17a8c664b9e38a5c63d4ff82
Umer Naeem <umernaeem135acc@gmail.com>
2026-01-09 18:05:28 +0500

    travel planner agent configured

--- apps/examples/mastra/src/mastra/index.ts
+++ apps/examples/mastra/src/mastra/index.ts
@@ -4,10 +4,11 @@ import { PinoLogger } from '@mastra/loggers';
 import { LibSQLStore } from '@mastra/libsql';
 import { weatherWorkflow } from './workflows/weather-workflow';
 import { weatherAgent } from './agents/weather-agent';
+import { travelPlannerAgent } from './agents/travel-planner-agent';
 
 export const mastra = new Mastra({
   workflows: { weatherWorkflow },
-  agents: { weatherAgent },
+  agents: { weatherAgent, travelPlannerAgent },
   storage: new LibSQLStore({
     // stores observability, scores, ... into memory storage, if it needs to persist, change to file:../mastra.db
     url: ":memory:",
@@ -16,10 +17,6 @@ export const mastra = new Mastra({
     name: 'Mastra',
     level: 'info',
   }),
-  telemetry: {
-    // Telemetry is deprecated and will be removed in the Nov 4th release
-    enabled: false,
-  },
   observability: {
     // Enables DefaultExporter and CloudExporter for AI tracing
     default: { enabled: true },
